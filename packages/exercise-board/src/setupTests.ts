c771eacd07d8499b071f970ac712251b82664381
nampdn <nampdn@protonmail.com>
2018-02-07 14:56:45 +0700

    chore(packages): refactor package exercise

--- packages/exercise-board/src/setupTests.ts
+++
@@ -1,8 +0,0 @@
-window.requestAnimationFrame = callback => {
-    setTimeout(callback, 0)
-    return 0
-}
-
-import * as Enzyme from 'enzyme'
-import * as Adapter from 'enzyme-adapter-react-16'
-Enzyme.configure({adapter: new Adapter()})
